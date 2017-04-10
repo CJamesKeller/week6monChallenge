95057c665fe5bbd3a248170567cf272015b16df8
Chris Keller <cjameskeller@gmail.com>
2017-04-10 15:18:35 -0500

    working

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -1,25 +1,32 @@
 var myApp = angular.module('myApp', []);
 
+myApp.controller("FirstController", ["$scope", "DataService", function($scope, DataService){
+  $scope.postData = DataService.postData;
+}]);
+
+myApp.controller("SecondController", ["$scope", "DataService", function($scope, DataService){
+  DataService.getData();
+  $scope.messageObject = DataService.messageObject;
+}]);
+
 myApp.factory('DataService', ['$http', function($http){
-    var messageObject = {
-      messages : []
-    };
+  var messageObject = {
+    messages: []
+  };
 
-    function getData(){
-      $http.get('/messages').then(function(response){
+  var getData = function(){
+    $http.get('/messages').then(function(response){
+      messageObject.messages = angular.copy(response.data);
+    });
+  };
 
-      });
-    }
+  function postData(message){
+    $http.post('/messages', message).then(getData());
+  }
 
-    function postData(message){
-      $http.post('/messages', message).then(function(response){
-
-      });
-    }
-
-    return {
-      messageObject : messageObject,
-      getData  : getData,
-      postData : postData
-    };
+  return {
+    messageObject : messageObject,
+    getData  : getData,
+    postData : postData
+  };
 }]);
